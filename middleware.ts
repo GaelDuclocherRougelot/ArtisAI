b773e06134d7274fca90220fe36852ecccd07ce4
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2024-09-16 17:20:15 +0200

    fix: types for build 3

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
-export default function handler(req: Request) {
-  try {
-    return clerkMiddleware()(req);
-  } catch (error) {
-    console.error("Middleware error:", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
-  }
-}
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };

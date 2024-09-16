a20b13ee61e6a40d11985551bba33ab996f04f96
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2024-09-16 23:28:22 +0200

    fix: db connection

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,12 @@ export const connectToDatabase = async () => {
 
   cached.promise =
     cached.promise ||
-    mongoose.connect(MONGODB_URL, { dbName: "artisai", bufferCommands: false });
+    mongoose.connect(MONGODB_URL, {
+      dbName: "artisai",
+      bufferCommands: false,
+    });
 
-  cached.conn = await cached.promise; 
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };

5bf5d50614540a634ac97049eb7101bd127ec693
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2024-09-19 16:31:10 +0200

    feat: add Cloudinary for background removal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
+import Header from "@/components/shared/Header";
+
 export default function Home() {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <Header title="Welcome back !" />
   );
 }
